81690f1baf61844934b6ad7c180bbbddcc5b31c6
caiden nason <caidenbnason@gmail.com>
2022-04-27 17:22:26 -0400

    commit tree b5a92560f42456f236b6d3abf8ea3de75c2e9e7c

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+function writeCards(arrayOfNames, events){
+    let newArr = []
+    for (let i = 0; i < arrayOfNames.length; i++){
+        newArr.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${events} gift!`)
+    }
+    return newArr
+}
+
+writeCards()
+
+function countDown(n){
+    let count = n
+    while (count >= 0){
+        console.log(count--)
+    }
+}
